977e08b6ba96be03a637d25ea3c4fe2081dd135e
dsgorthy <derek.gorthy@colorado.edu>
2017-04-18 19:32:00 -0600

    Implemented extended owner class, need game class to be finished before completing many of these functions.

---
+++ code/Owner.java
@@ -0,0 +1,44 @@
+/******************************************************************************
+ *  Author(s):    Derek Gorthy
+ *
+ *  Compilation:  javac Owner.java
+ *  
+ *  Implements the Owner class.
+ *
+ ******************************************************************************/
+
+import java.util.*;
+
+public class Owner extends User {
+	private String activeBusinessID;
+
+	public ArrayList<String> businesses;
+
+	Owner(String username, String password, double balance) {
+		super(username, password, balance);
+		this.businesses = new ArrayList<String>();
+	}
+
+	// change to private
+	//public void removeUser(String user) {
+	//}
+
+	// Implement game first
+	// change to private
+	//public void buyBusiness(String busType, String busName){
+	//}
+
+	// Implement game first
+	// change to private
+	//public void sellBusiness(String busType, String busName){
+	//}
+
+	public String getBusinessID() {
+		return activeBusinessID;
+	}
+
+	// TODO: figure out where to find the business object, only passing in the ID
+	//public void toggleOpenClose(String businessID) {
+	//	businessID.changeOpenStatus();
+	//}
+}
