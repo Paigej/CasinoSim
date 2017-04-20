a4ec95c812cf8395c1eb85d04e17c978fd7ab4f2
dsgorthy <derek.gorthy@colorado.edu>
2017-04-19 21:05:37 -0600

    Changed from interface to an abstract class. Possibly wrong, but leaving it for now.

--- code/Game.java
+++ code/Game.java
@@ -7,8 +7,28 @@
  *
  ******************************************************************************/
 
-public interface Game {
+public abstract class Game {
 
-	public String getID();
-	public void playGame(String playerID, double bet);
+	private String id;
+	private String casinoID;
+	// Figure out the probability table --> private probTable;
+
+	public String name;
+	public double costToBuild;
+	public int seatsAvailable;
+	public int seatsOccupied;
+
+	Game(String id, String casinoID, String name) {
+		this.id = id;
+		this.casinoID = casinoID;
+		this.name = name;
+	}
+
+	public String getID() {
+		return id;
+	}
+
+	public void playGame(String playerID, double bet) {
+		System.out.print("It looks like playGame was not implemented correctly.")
+	}
 }
