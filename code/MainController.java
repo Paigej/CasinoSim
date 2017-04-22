d66b678c3e6232a7c2ca2425c8c2704dc6f0565c
Michael Condon <mico0399@colorado.edu>
2017-04-21 22:23:47 -0600

    Added intital message and log in option (Command-Line UI)

--- code/MainController.java
+++ code/MainController.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /******************************************************************************
  *  Author(s):    Michael Condon
  *
@@ -7,11 +9,63 @@
  *
  ******************************************************************************/
 public class MainController {
+	
+	private static Scanner scan;
 
-	public static void main(String[] args) {
+	/*
+	 * Intial message to user
+	 */
+	public static void printIntroMessage(){
+		System.out.println("Welcome to CasinoSim!");
+		System.out.println("Log in or Sign up?");
+	}
+
+	/*
+	 * Allows user to select whether they are signing up or logging in
+	 * will repeat until user has chosen a valid option
+	 * returns a number indicating user's selection
+	 * 1 for log in screen
+	 * 2 for sign up
+	 */
+	public static int choseLogIn(){
+		scan = new Scanner(System.in);
+		String userSelection = scan.nextLine();// Scans the next token of the input as a String.
 		
-		Owner Bob = new Owner("Bob", "bob1978", 10000);
-		System.out.println(Bob.getBalance());
+		if (userSelection.indexOf("Log") != -1 || userSelection.indexOf("log") != -1)
+		{
+			return 1;
+		}
+		else if (userSelection.indexOf("Sign") != -1 || userSelection.indexOf("sign") != -1)
+		{
+			return 2;
+		}
+		else
+		{
+			System.out.println("Please type 'Log in' or 'Sign up'");
+			return choseLogIn();
+		}
+	}
+	
+	public static void logIn(){
+		
+	}
+
+	public static void signUp(){
+		
+	}
+	
+	public static void main(String[] args) {
+		printIntroMessage();
+		int nextView = choseLogIn();
+		if (nextView == 1)
+		{
+			logIn();
+		}
+		else if (nextView == 2)
+		{
+			signUp();
+		}
+		
 	}
 
 }
